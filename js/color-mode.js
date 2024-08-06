581ea4df09b28d1474d9d52876fca5768c85b3f2
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2024-08-06 20:32:36 +0200

    Add files via upload

--- js/color-mode.js
+++ js/color-mode.js
@@ -2,9 +2,9 @@
 const toggleButton = document.querySelector("#toggle-button");
 const root = document.querySelector(":root");
 const storageKey = "color-mode";
-const defaultMode = "light-mode";
+const defaultMode = "dark-mode"; // Establecemos el modo oscuro como el predeterminado
 
-// Load the user's preffered color mode from local storage.
+// Load the user's preferred color mode from local storage.
 function loadColorMode() {
   const colorMode = localStorage.getItem(storageKey);
   root.classList.add(colorMode || defaultMode);
@@ -14,26 +14,28 @@ function loadColorMode() {
 loadColorMode();
 
 // Toggle the color mode
-toggleButton.addEventListener("click", () => {
-  saveColorMode();
-});
+// Comentado para evitar que el usuario pueda cambiar el modo
+// toggleButton.addEventListener("click", () => {
+//   saveColorMode();
+// });
 
-// Save the users's preffered color mode to local storage
-function saveColorMode() {
-  // Check if the root element has a class of "dark-mode". If yes, then the current mode is switched to light, and vice versa.
-  const currentMode = root.classList.contains("dark-mode")
-    ? "light-mode"
-    : "dark-mode";
-  root.classList.remove("light-mode", "dark-mode");
-  root.classList.add(currentMode);
-  localStorage.setItem(storageKey, currentMode);
-  updateToggleButton();
-}
+// Save the user's preferred color mode to local storage
+// Comentado ya que no queremos guardar o alternar el modo de color
+// function saveColorMode() {
+//   const currentMode = root.classList.contains("dark-mode")
+//     ? "light-mode"
+//     : "dark-mode";
+//   root.classList.remove("light-mode", "dark-mode");
+//   root.classList.add(currentMode);
+//   localStorage.setItem(storageKey, currentMode);
+//   updateToggleButton();
+// }
 
 function updateToggleButton() {
-  if (root.classList.contains("dark-mode")) {
-    toggleButton.style.backgroundImage = "var(--moon)";
-  } else {
-    toggleButton.style.backgroundImage = "var(--sun)";
-  }
+  // Comentado ya que no se usa en esta versión que he hecho sin cambio de modo, pero lo mantengo por si en un futuro cambio de idea
+  // if (root.classList.contains("dark-mode")) {
+  //   toggleButton.style.backgroundImage = "var(--moon)";
+  // } else {
+  //   toggleButton.style.backgroundImage = "var(--sun)";
+  // }
 }
